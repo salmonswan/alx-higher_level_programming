c54d279cbdd38e1db69bd2307b6456776a2573ec
Toliva Solomon Iwutung <tisopar@gmail.com>
2022-07-03 22:59:01 +0300

    update script

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-print("{:d} + {:d} = {:d}".format(a, b, a + b))
-print("{:d} - {:d} = {:d}".format(a, b, a - b))
-print("{:d} * {:d} = {:d}".format(a, b, a * b))
-print("{:d} / {:d} = {:d}".format(a, b, int(a / b)))
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, a + b))
+    print("{:d} - {:d} = {:d}".format(a, b, a - b))
+    print("{:d} * {:d} = {:d}".format(a, b, a * b))
+    print("{:d} / {:d} = {:d}".format(a, b, int(a / b)))
