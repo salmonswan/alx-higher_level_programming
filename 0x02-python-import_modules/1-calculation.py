260e868b6f23408f0a81e9a27bb8836692a785e5
Toliva Solomon Iwutung <tisopar@gmail.com>
2022-07-03 22:48:29 +0300

    add script

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+print("{:d} + {:d} = {:d}".format(a, b, a + b))
+print("{:d} - {:d} = {:d}".format(a, b, a - b))
+print("{:d} * {:d} = {:d}".format(a, b, a * b))
+print("{:d} / {:d} = {:d}".format(a, b, int(a / b)))
