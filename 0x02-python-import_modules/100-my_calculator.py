58e73602b38a1508ab4f65ef3fb616d8a48013a8
Toliva Solomon Iwutung <tisopar@gmail.com>
2022-07-04 18:43:55 +0300

    ad script

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from ast import operator
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import *
+    if len(argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        a, opp, b = int(argv[1]), argv[2], int(argv[3])
+        if opp == '+':
+            print("{} + {} = {}".format(a, b, add(a, b)))
+            exit(0)
+        elif opp == '-':
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+            exit(0)
+        elif opp == '*':
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+            exit(0)
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
