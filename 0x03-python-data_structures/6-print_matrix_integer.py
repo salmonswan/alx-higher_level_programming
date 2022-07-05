dd7ce987c08c0531cd84483712464dc23779f83b
Toliva Solomon Iwutung <tisopar@gmail.com>
2022-07-05 08:27:15 +0300

    update script

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            print("{}".format(j), end=(" " if i.index(j) != i[-1] else ""))
+            print("{:d}".format(j), end=(" " if i.index(j) != i[-1] else ""))
         print()
