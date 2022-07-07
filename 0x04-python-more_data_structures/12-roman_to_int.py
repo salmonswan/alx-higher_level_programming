79dacb444934535f6c9444d103e27e260126e0b4
Toliva Solomon Iwutung <tisopar@gmail.com>
2022-07-07 14:49:53 +0300

    update script

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    units = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
-    tens = ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
-    hundreds = ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
-    thousands = ['M', 'MM', 'MMM']
-    roman = units.copy()
-    for x in tens:
-        roman.append(x)
-        for i in units:
-            roman.append(x + i)
+    if roman_string.type() != str or roman_string is None:
+        return (0)
+    else:
+        units = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+        tens = ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
+        hundreds = ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+        thousands = ['M', 'MM', 'MMM']
+        roman = units.copy()
+        for x in tens:
+            roman.append(x)
+            for i in units:
+                roman.append(x + i)
 
-    roman2 = roman.copy()
-    for c in hundreds:
-        roman2.append(c)
-        for x in roman:
-            roman2.append(c + x)
+        roman2 = roman.copy()
+        for c in hundreds:
+            roman2.append(c)
+            for x in roman:
+                roman2.append(c + x)
 
-    roman3 = roman2.copy()
-    for m in thousands:
-        roman3.append(m)
-        for c in roman2:
-            roman3.append(m + c)
+        roman3 = roman2.copy()
+        for m in thousands:
+            roman3.append(m)
+            for c in roman2:
+                roman3.append(m + c)
 
-    dict_list = []
-    for i, j in zip(roman3, list(range(1, len(roman3) + 1))):
-        dict_list.append((i, j))
-    num_dict = dict(dict_list)
+        dict_list = []
+        for i, j in zip(roman3, list(range(1, len(roman3) + 1))):
+            dict_list.append((i, j))
+        num_dict = dict(dict_list)
 
-    return(num_dict[roman_string])
+        return(num_dict[roman_string])
