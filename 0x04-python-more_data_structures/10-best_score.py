82e086b762d1ad0456517cab60dcd66dd492c688
Toliva Solomon Iwutung <tisopar@gmail.com>
2022-07-07 10:49:35 +0300

    add script

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return (None)
+    else:
+        best_score = list(a_dictionary.keys())[0]
+        for key, value in a_dictionary.items():
+            if value > a_dictionary[best_score]:
+                best_score = key
+    return (best_score)
