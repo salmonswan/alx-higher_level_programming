4d0fe87798b8f0893a06078a1222a70e5e30c0d8
Toliva Solomon Iwutung <tisopar@gmail.com>
2022-07-07 10:56:05 +0300

    update script

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary == {} or a_dictionary is None:
         return (None)
     else:
         best_score = list(a_dictionary.keys())[0]
