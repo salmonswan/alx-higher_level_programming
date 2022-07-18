aa0b1c58bdcb8cdbac10cb144b525565485b4709
Toliva Solomon Iwutung <tisopar@gmail.com>
2022-07-19 01:29:08 +0300

    add script

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+def list_division(my_list_1, my_list_2, list_length):
+    new = []
+    for i in range(list_length):
+        try:
+            _div = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            _div = 0
+        except TypeError:
+            print("wrong type")
+            _div = 0
+        except IndexError:
+            print("out of range")
+            _div = 0
+        finally:
+            new.append(_div)
+    return (new)
