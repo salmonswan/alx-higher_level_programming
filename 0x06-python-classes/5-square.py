b7af0860253fdcd92c1bdd206ad3f96c8bb802e4
Toliva Solomon Iwutung <tisopar@gmail.com>
2022-07-19 22:44:45 +0300

    add script

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''Defining a class Square Defined by size'''
+
+
+class Square:
+    '''Defines a square'''
+
+    def __init__(self, size=0):
+        '''
+        Initializes a square
+
+        Args:
+            size: size of the square
+        '''
+        self.size = size
+
+    @property
+    def size(self):
+        '''retreive the size of the square'''
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        '''define the area of the square'''
+        return(self.__size**2)
+
+    def my_print(self):
+        '''prints the square'''
+        for i in range(0, self.__size):
+            for j in range(0, self.__size):
+                print("#", end="")
+            print()
+        if self.__size == 0:
+            print()
