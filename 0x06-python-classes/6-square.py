e80eefb9575c6aabdf9dcc6f668074a6d1828f93
Toliva Solomon Iwutung <tisopar@gmail.com>
2022-07-19 22:50:56 +0300

    add script

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+'''Defining a class Square Defined by size'''
+
+
+from turtle import position
+
+
+class Square:
+    '''Defines a square'''
+
+    def __init__(self, size=0, position=(0, 0)):
+        '''
+        Initializes a square
+
+        Args:
+            size: size of the square
+        '''
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        '''retreive the size of the square'''
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        """Get/set the current position of the square."""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        '''define the area of the square'''
+        return(self.__size**2)
+
+    def my_print(self):
+        '''prints the square'''
+        for i in range(0, self.__size):
+            for j in range(0, self.__size):
+                print("#", end="")
+            print()
+        if self.__size == 0:
+            print()
