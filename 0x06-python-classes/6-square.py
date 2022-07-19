21a13743919ce505636244be4557534641c9a74c
Toliva Solomon Iwutung <tisopar@gmail.com>
2022-07-19 22:56:08 +0300

    update script

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,9 +49,12 @@ class Square:
 
     def my_print(self):
         '''prints the square'''
-        for i in range(0, self.__size):
-            for j in range(0, self.__size):
-                print("#", end="")
-            print()
         if self.__size == 0:
+            print()
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
             print()
