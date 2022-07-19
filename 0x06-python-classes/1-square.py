5b034aed2a2d28e2507784013fad901737e860fb
Toliva Solomon Iwutung <tisopar@gmail.com>
2022-07-19 21:14:01 +0300

    add script

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Defining a class Square Defined by size'''
+
+
+class Square:
+    '''Defines a square'''
+
+    def __init__(self, size):
+        '''
+        Initializes a square
+
+        Args:
+            size: size of the square
+        '''
+        self.__size = size
